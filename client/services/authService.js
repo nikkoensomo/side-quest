26889a9213125e672628cbf52451a70dee809770
Nikko Ensomo <nikkonuriel@gmail.com>
2026-04-28 20:08:17 +0800

    feat: added authentication services

---
+++ client/services/authService.js
@@ -0,0 +1,11 @@
+import api from '../src/api/axios.js';
+
+export const signupService = async (formData) => {
+    const response = await api.post('/auth/signup', formData);
+    return response.data;
+}
+
+export const loginService = async (formData) => {
+    const response = await api.post('/aut/login', formData);
+    return response.data;
+}
