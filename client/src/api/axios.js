69992439fda96855b82ac5dafc8addd9a85a510b
Nikko Ensomo <nikkonuriel@gmail.com>
2026-05-01 00:06:24 +0800

    fix: resolved displaying user issue'

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -7,6 +7,7 @@ const api = axios.create({
 // automatically attach token to every request
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
+    console.log('sending token:', token);
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
--- client/src/components/headers/DashboardHeader.jsx
+++ client/src/components/headers/DashboardHeader.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Bell } from 'lucide-react';
+import { getLoggedInUser } from '../../services/userService';
 
 const DashboardHeader = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        async function fetchUser() {
+            try {
+                const data = await getLoggedInUser();
+                console.log(data);
+                setUser(data);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchUser();
+    }, [])
+
     return (
         <>
             <header className="w-full bg-white px-6 py-4 flex flex-row justify-between items-center">
                 <span className="text-black text-2xl font-bold">SideQuest</span>
                 <div className="flex gap-6">
-                    <Bell size={24}/>
+                    <Bell size={24} />
                     <div className="flex flex-col">
-                        <span className="text-sm font-medium">James Ian Dayuno</span>
+                        {user ? (
+                            <span className="text-sm font-medium">{user.username}</span>
+                        ) : (
+                            <span>Loading...</span>
+                        )}
                     </div>
                 </div>
             </header>
