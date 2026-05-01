aeea163c12d406905916c39125dfe2c2bb60cab1
Nikko Ensomo <nikkonuriel@gmail.com>
2026-05-02 01:22:15 +0800

    feat: made changes to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,16 @@ import LandingPage from './pages/LandingPage'
 import AboutUsPage from './pages/AboutUsPage'
 import ContactUsPage from './pages/ContactUsPage'
 import DashboardPage from './pages/DashboardPage'
+import { DashboardLayout } from './layouts/DashboardLayout'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/landing-page" element={<LandingPage></LandingPage>}/>
-        <Route path="/dashboard-page" element={<DashboardPage></DashboardPage>}/>
-
+        <Route path="/landing-page" element={<LandingPage></LandingPage>} />
+        <Route path="/dashboard-page" element={<DashboardLayout></DashboardLayout>} />
+          <Route index element={<DashboardPage></DashboardPage>} />
         <Route path="/about-us-page" element={<AboutUsPage></AboutUsPage>} />
         <Route path="/contact-us-page" element={<ContactUsPage></ContactUsPage>} />
       </Routes>
