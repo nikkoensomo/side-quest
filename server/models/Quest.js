e8636ca9f967b15ea8a922a18d5bf5a8aa200f7d
Nikko Ensomo <nikkonuriel@gmail.com>
2026-05-18 21:53:11 +0800

    refactor: added more statuses to status

--- server/models/Quest.js
+++ server/models/Quest.js
@@ -39,7 +39,7 @@ const questSchema = new mongoose.Schema({
     },
     status: {
         type: String,
-        enum: ['open', 'in-progress', 'completed'],
+        enum: ['open', 'accepted', 'in-progress', 'completed', 'cancelled'],
         default: 'open',
         required: true,
     }
