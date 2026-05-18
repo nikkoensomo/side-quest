7896425f0452c7c992c6bbe88a120bd1c394f7b2
Nikko Ensomo <nikkonuriel@gmail.com>
2026-05-18 18:44:41 +0800

    feat: added quest cards into the dashboard page

--- client/src/components/cards/QuestCard.jsx
+++ client/src/components/cards/QuestCard.jsx
@@ -9,7 +9,7 @@ const QuestCard = ({
 }) => {
     return (
         <>
-            <div className="w-full bg-white rounded-lg shadow-sm cursor-pointer hover:shadow-md transition-shadow duration-200">
+            <div className="w-full bg-white px-4 py-2 rounded-lg shadow-sm cursor-pointer hover:shadow-md transition-shadow duration-200">
                 <div className="flex flex-col gap-4 justify-center items-center">
                     <p className="text-black font-semibold text-2xl">Title</p>
                     <span className="text-gray-400 font-medium">Pickup Location</span>
--- client/src/components/cards/QuestList.jsx
+++ client/src/components/cards/QuestList.jsx
@@ -1,20 +1,24 @@
 import QuestCard from "./QuestCard";
 
 const QuestList = ({ quests }) => {
+    if (!quests) return null;
+
     return (
         <>
-            {quests.map((quest) => (
-                <QuestCard 
-                    key={quest._id}
-                    title={quest.title}
-                    description={quest.description}
-                    status={quest.status}
-                    postedBy={quest.postedBy}
-                    pickupLocation={quest.locatio}
-                    deliveryLocation={quest.deliveryLocation}
-                    reward={quest.reward}
-                />
-            ))}
+            <div className="grid grid-cols-3 gap-4">
+                {quests.map((quest) => (
+                    <QuestCard
+                        key={quest._id}
+                        title={quest.title}
+                        description={quest.description}
+                        status={quest.status}
+                        postedBy={quest.postedBy}
+                        pickupLocation={quest.locatio}
+                        deliveryLocation={quest.deliveryLocation}
+                        reward={quest.reward}
+                    />
+                ))}
+            </div>
         </>
     )
 }
