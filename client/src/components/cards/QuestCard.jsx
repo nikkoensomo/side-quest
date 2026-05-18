596bda31b3220a77cf426b150e315302f4b61b7e
Nikko Ensomo <nikkonuriel@gmail.com>
2026-05-18 19:27:58 +0800

    feat: added more elements in quest card

--- client/src/components/cards/QuestCard.jsx
+++ client/src/components/cards/QuestCard.jsx
@@ -11,14 +11,16 @@ const QuestCard = ({
         <>
             <div className="w-full bg-white px-4 py-2 rounded-lg shadow-sm cursor-pointer hover:shadow-md transition-shadow duration-200">
                 <div className="flex flex-col gap-4 justify-center items-center">
-                    <p className="text-black font-semibold text-2xl">Title</p>
-                    <span className="text-gray-400 font-medium">Pickup Location</span>
+                    <p className="text-black font-semibold text-2xl">{title}</p>
+                    <span className="text-gray-400 font-medium">{pickupLocation}</span>
 
-                    <hr></hr>
-                    <div className="flex justify-between items-center">
-                        <span className="text-black font-medium">Posted By</span>
-                        <span className="text-black font-medium">Status</span>
+                    <hr className="w-full border-gray-300"></hr>
+                    <div className="flex justify-between items-center gap-4">
+                        <span className="text-black font-medium">{postedBy}</span>
+                        <span className="text-black font-medium">{status}</span>
                     </div>
+
+                    <span className="text-black font-medium">{reward}</span>
                 </div>
             </div>
         </>
