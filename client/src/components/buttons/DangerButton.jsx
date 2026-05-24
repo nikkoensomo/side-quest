56df10b6b49b03c37abf5fb3e40be7f05aad8229
Nikko Ensomo <nikkonuriel@gmail.com>
2026-05-24 21:16:36 +0800

    refactor: changed danger button border radius to lg

--- client/src/components/buttons/DangerButton.jsx
+++ client/src/components/buttons/DangerButton.jsx
@@ -4,7 +4,7 @@ const DangerButton = ({ label, type, onClick }) => {
             <button
                 type={type}
                 onClick={onClick}
-                className="bg-red-400 text-white px-4 py-2 rounded-full hover:bg-red-600 cursor-pointer"
+                className="bg-red-400 text-white px-4 py-2 rounded-lg hover:bg-red-600 cursor-pointer"
             >
                 {label}
             </button>
