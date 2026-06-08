e3c55eeb4f8438f30b20d99451dca534b0340681
Nikko Ensomo <nikkonuriel@gmail.com>
2026-06-08 21:37:47 +0800

    feat: added deleteUserAccountService

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,3 +4,8 @@ export const getLoggedInUser = async () => {
     const response = await api.get('/users/logged-user');
     return response.data;
 }
+
+export const deleteUserAccountService = async (userId) => {
+    const response = await api.delete('/users');
+    return response.data;
+}
