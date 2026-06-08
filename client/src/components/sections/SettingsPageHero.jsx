287058531b030a5975938566561b75b52ada2ae4
Nikko Ensomo <nikkonuriel@gmail.com>
2026-06-08 19:36:05 +0800

    feat: added initial settings page layout

---
+++ client/src/components/sections/SettingsPageHero.jsx
@@ -0,0 +1,11 @@
+const SettingsPageHero = () => {
+    return (
+        <>
+            <section className="w-full flex flex-col justify-center items-center">
+                <span className="text-black text-2xl">This is the settings page hero.</span>
+            </section>
+        </>
+    )
+}
+
+export default SettingsPageHero;
