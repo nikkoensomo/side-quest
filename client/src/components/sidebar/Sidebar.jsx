0d5d25497b63b5db73c6a3d2cac851b2865b0a42
Nikko Ensomo <nikkonuriel@gmail.com>
2026-06-08 18:44:59 +0800

    feat: added sidebar arrow icon

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { LayoutDashboard, CheckSquare, Settings, User, LogOutIcon } from "lucide-react";
+import { LayoutDashboard, CheckSquare, Settings, User, LogOutIcon, ArrowLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useState } from 'react';
 import SidebarNavItem from "./SidebarNavItem";
@@ -30,7 +30,18 @@ const Sidebar = () => {
     return (
         <>
             <aside className="h-screen w-64 bg-white border-r border-gray-200 flex flex-col px-4 py-6">
-                <span className="text-black text-2xl font-bold px-4 mb-4">SideQuest</span>
+                <div className="w-full flex justify-between items-center mb-4">
+                    <span className="text-black text-2xl font-bold px-4">SideQuest</span>
+                    <button
+                        className="rounded-md p-1 hover:bg-gray-300"
+                    >
+                        <ArrowLeft
+                            className="cursor-pointer"
+                            size={20}
+                        />
+                    </button>
+                </div>
+
 
                 <nav className="flex flex-1 flex-col justify-between">
                     <div className="w-full flex flex-col gap-1">
@@ -53,7 +64,7 @@ const Sidebar = () => {
                 </nav>
             </aside>
 
-            <LogoutConfirmationModal 
+            <LogoutConfirmationModal
                 isOpen={modalMode === 'logout'}
                 text='Are you sure you want to logout?'
                 label='Logout'
