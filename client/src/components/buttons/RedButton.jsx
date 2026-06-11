2658f22b240a0eadf307aca252e085b02b2e52af
Nikko Ensomo <nikkonuriel@gmail.com>
2026-06-11 21:40:46 +0800

    feat: added RedButton component

---
+++ client/src/components/buttons/RedButton.jsx
@@ -0,0 +1,15 @@
+const RedButton = ({ type, label, onClick }) => {
+    return (
+        <>
+            <button
+                type={type}
+                onClick={onClick}
+                className="bg-red-400 text-white rounded-lg cursor-pointer hover:bg-red-600"
+            >
+                {label}
+            </button>
+        </>
+    )
+}
+
+export default RedButton;
