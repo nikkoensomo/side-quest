3824ce370e29a0b292b40254da84e27042774d17
Nikko Ensomo <nikkonuriel@gmail.com>
2026-06-11 22:09:24 +0800

    feat: added Delete Account button in settings page

--- client/src/components/buttons/RedButton.jsx
+++ client/src/components/buttons/RedButton.jsx
@@ -4,7 +4,7 @@ const RedButton = ({ type, label, onClick }) => {
             <button
                 type={type}
                 onClick={onClick}
-                className="bg-red-400 text-white rounded-lg cursor-pointer hover:bg-red-600"
+                className="bg-red-400 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-red-600"
             >
                 {label}
             </button>
--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,11 +1,16 @@
 import SettingsPageHero from "../components/sections/SettingsPageHero";
+import RedButton from "../components/buttons/RedButton";
 
 const SettingsPage = () => {
     return (
         <>
             <SettingsPageHero />
-            <main>
+            <main className="w-full flex flex-col gap-4 items-start justify-center">
                 <span>settings page yeah!</span>
+                <RedButton 
+                    type="button"
+                    label="Delete Account"
+                />
             </main>
         </>
     )
