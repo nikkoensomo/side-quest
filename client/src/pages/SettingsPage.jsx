4d917b0296209fc2422c3085ee0d78bbf14597c2
Nikko Ensomo <nikkonuriel@gmail.com>
2026-06-17 22:49:09 +0800

    feat: added initial settingsPage logic

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+import { getLoggedInUser } from '../services/userService';
 import SettingsPageHero from "../components/sections/SettingsPageHero";
 import RedButton from "../components/buttons/RedButton";
 
 const SettingsPage = () => {
+    const [modalMode, setModalMode] = useState(null);
+    const [selectedAccount, setSelectedAccount] = useState(null);
+ 
+    const handleDeleteModal = () => {
+
+    }
+
+    useEffect(() => {
+        async function getLoggedInAccount() {
+            try {
+                const currentUser = await getLoggedInUser();
+
+                console.log(currentUser);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        getLoggedInAccount();
+    }, []);
+
     return (
         <>
             <SettingsPageHero />
@@ -10,6 +33,7 @@ const SettingsPage = () => {
                 <RedButton 
                     type="button"
                     label="Delete Account"
+                    
                 />
             </main>
         </>
